35e3f8a00a9f9a479dada6e7c59d9517d67047f9
SzymonPobiega <szymon.pobiega@gmail.com>
2016-08-24 09:14:04 +0200

    Added acceptance tests.

---
+++ src/NServiceBus.AcceptanceTests/Routing/When_subscribing_to_a_derived_event_with_a_route_for_base_event.cs
@@ -0,0 +1,76 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Features;
+    using NUnit.Framework;
+
+    public class When_subscribing_to_a_derived_event_with_a_route_for_base_event : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Event_should_be_delivered()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<Publisher>(b => b.When(c => c.SubscriberSubscribed, async session =>
+                {
+                    await session.Publish(new SpecificEvent());
+                }))
+                .WithEndpoint<Subscriber>(b => b.When(async (session, c) => await session.Subscribe<SpecificEvent>()))
+                .Done(c => c.SubscriberGotEvent)
+                .Run();
+
+            Assert.True(context.SubscriberGotEvent);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool SubscriberGotEvent { get; set; }
+
+            public bool SubscriberSubscribed { get; set; }
+        }
+
+        public class Publisher : EndpointConfigurationBuilder
+        {
+            public Publisher()
+            {
+                EndpointSetup<DefaultPublisher>(b => b.OnEndpointSubscribed<Context>((args, context) =>
+                {
+                    context.SubscriberSubscribed = true;
+                }));
+            }
+        }
+        
+        public class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.DisableFeature<AutoSubscribe>();
+                    c.LimitMessageProcessingConcurrencyTo(1); //To ensure Done is processed after the event.
+                })
+                    .AddMapping<IBaseEvent>(typeof(Publisher));
+            }
+
+            public class MyEventHandler : IHandleMessages<SpecificEvent>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(SpecificEvent messageThatIsEnlisted, IMessageHandlerContext context)
+                {
+                    Context.SubscriberGotEvent = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        public class SpecificEvent : IBaseEvent
+        {
+        }
+
+        public interface IBaseEvent : IEvent
+        {
+        }
+    }
+}
